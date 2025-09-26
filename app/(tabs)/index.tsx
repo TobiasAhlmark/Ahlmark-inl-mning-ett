16b801f2aea24b1bd9bc06f108dce4be7381a9ff
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-09-26 13:32:36 +0200

    Created homepage for the game with touchable pictures and animations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,56 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { UseRockPaper } from "@/hooks/usePaperRock";
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function GameScreen() {
+  const { userChoice, displayChoice, result, PlayRound } = UseRockPaper();
+
+  const images = {
+    Rock: require("../../assets/images/rock.png"),
+    Scissor: require("../../assets/images/scissor.png"),
+    Bag: require("../../assets/images/bag.png"),
+  };
+
+  const imagesSvenska = {
+    Sten: require("../../assets/images/rock.png"),
+    Sax: require("../../assets/images/scissor.png"),
+    Påse: require("../../assets/images/bag.png"),
+  };
+
+  const choiceEmojis = {
+    Sten: "✊",
+    Sax: "✌️",
+    Påse: "🖐️",
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Spelet</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <View style={styles.pressAbleView}>
+        <TouchableOpacity onPress={() => PlayRound("Sten")}>
+          <Image source={images.Rock} style={styles.images} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => PlayRound("Sax")}>
+          <Image source={images.Scissor} style={styles.images} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => PlayRound("Påse")}>
+          <Image source={images.Bag} style={styles.images} />
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.info}>Du valde: </Text>
+
+      <Image
+        source={imagesSvenska[userChoice as keyof typeof imagesSvenska]}
+        style={styles.images}
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      <Text style={styles.info}>Datorn visar: </Text>
+
+      <Image
+        source={imagesSvenska[displayChoice as keyof typeof displayChoice]}
+        style={styles.images}
+      />
+      <Text>Resultat: {result}</Text>
     </View>
   );
 }
@@ -22,14 +60,38 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 32,
     fontWeight: "bold",
+    marginBottom: 20,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  choices: {
+    flexDirection: "row",
+    gap: 30,
+    marginBottom: 30,
+  },
+  choice: {
+    fontSize: 60, // stora emojis
+  },
+  info: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  result: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+  images: {
+    borderRadius: 15,
+    width: 100,
+    height: 140,
+    marginBottom: 10,
+    marginRight: 10,
+  },
+  pressAbleView: {
+    flexDirection: "row",
   },
 });
