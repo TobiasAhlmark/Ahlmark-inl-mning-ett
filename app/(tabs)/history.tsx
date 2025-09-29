d2dc07fb0ad13134cfd6a4a9bfbc22fbeb3a40c2
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-09-29 11:24:47 +0200

    created historyScreen

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,22 +1,51 @@
+import { historyAtom } from "@/atoms/historyAtoms";
+import { Text, View } from "@/components/Themed";
+import { supabase } from "@/lib/supaBase";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-import { gameHistory } from "@/data";
+export default async function HistoryScreen() {
+  const [result, setResult] = useAtom(historyAtom);
 
-export default function HistoryScreen() {
+  useEffect(() => {
+    const fetchResults = async () => {
+      const { data, error } = await supabase
+        .from("results")
+        .select(
+          "id, player_name, user_choice, computer_choice, result, created_at"
+        )
+        .order("created_at", { ascending: false })
+        .limit(25);
+
+      if (error) {
+        console.error("❌ Fetch error:", error.message);
+      } else {
+        setResult(data);
+      }
+    };
+
+    fetchResults();
+  }, [setResult]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>History</Text>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={styles.title}>Historik</Text>
+        <Text style={styles.title}>Historik</Text>
+        <Text style={styles.title}>Historik</Text>
+        <Text style={styles.title}>Historik</Text>
+      </View>
       <FlatList
-        data={gameHistory}
+        data={result}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <Text>
-            {item.id}. Resultat: {item.result} ({item.player} - {item.opponent})
+            {item.player_name}. Ditt val: {item.user_choice} Datorns val:{" "}
+            {item.computer_choice} Resultat: {item.result}
           </Text>
         )}
       ></FlatList>
-      bu
     </View>
   );
 }
