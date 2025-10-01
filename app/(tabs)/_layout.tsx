d181fd341c4d4abaa2d5f4fd1223a64a8bf24d73
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-10-01 18:04:35 +0200

    Refactor layout to reduce code duplication and improve reusability

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,14 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import React from "react";
-import { Pressable } from "react-native";
-
-import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 import { useColorScheme } from "@/components/useColorScheme";
 import Colors from "@/constants/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { Link, withLayoutContext } from "expo-router";
+import React from "react";
+import { Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const { Navigator } = createMaterialTopTabNavigator();
+export const Tabs = withLayoutContext(Navigator);
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>["name"];
@@ -14,104 +17,77 @@ function TabBarIcon(props: {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
+function InfoButton() {
+  const colorScheme = useColorScheme();
+  return (
+    <Link href="/modal" asChild>
+      <Pressable>
+        {({ pressed }) => (
+          <FontAwesome
+            name="info-circle"
+            size={22}
+            color={Colors[colorScheme ?? "light"].text}
+            style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+          />
+        )}
+      </Pressable>
+    </Link>
+  );
+}
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: useClientOnlyValue(false, true),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Sten - Sax - Påse",
-          tabBarIcon: ({ color }) => <TabBarIcon name="play" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tabs
+        tabBarPosition="bottom"
+        screenOptions={{
+          swipeEnabled: true,
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-          tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="history"
-        options={{
-          title: "Single games",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="history" color={color} />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="bestOfThreeHistory"
-        options={{
-          title: "Best of three",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="history" color={color} />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Play Game",
+            headerRight: () => <InfoButton />,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <TabBarIcon name="play" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            headerRight: () => <InfoButton />,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <TabBarIcon name="book" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="history"
+          options={{
+            title: "Single games",
+            headerRight: () => <InfoButton />,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <TabBarIcon name="history" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bestOfThreeHistory"
+          options={{
+            title: "Best of three",
+            headerRight: () => <InfoButton />,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <TabBarIcon name="history" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
