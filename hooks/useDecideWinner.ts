a6f82f55f58fb1d5cbe337207e1309854fae8eea
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-10-02 08:40:18 +0200

    Used validation from dbinserts in useDecideWinner istead of in dbservices

--- hooks/useDecideWinner.ts
+++ hooks/useDecideWinner.ts
@@ -1,52 +1,75 @@
 import { computerScoreAtom, playerScoreAtom } from "@/atoms/gameAtoms";
 import { playerNameAtom } from "@/atoms/historyAtoms";
-import { dbService } from "@/services/dbService";
+import { dbServiceInsertBestOf } from "@/services/dbServices";
 import * as Haptics from "expo-haptics";
 import { useAtom } from "jotai";
 import { useEffect } from "react";
-
 
 export function useDecideWinner() {
   const [playerScore, setPlayerScore] = useAtom(playerScoreAtom);
   const [computerScore, setComputerScore] = useAtom(computerScoreAtom);
   const [playerName] = useAtom(playerNameAtom);
 
-    function decideWinner(userC: string, computerC: string) {
+  function decideWinner(userC: string, computerC: string) {
+    if (userC === computerC) {
+      Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
 
-  if (userC === computerC) {
-    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
-    
-    return "🖖: Oavgjort";
-  }
-  if (
-    (userC === "Sten" && computerC === "Sax") ||
-    (userC === "Sax" && computerC === "Påse") ||
-    (userC === "Påse" && computerC === "Sten")
-  ) {
-    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
-    setPlayerScore(p => p + 1);
-    return "WIN";
-  }
+      return "🖖: Oavgjort";
+    }
+    if (
+      (userC === "Sten" && computerC === "Sax") ||
+      (userC === "Sax" && computerC === "Påse") ||
+      (userC === "Påse" && computerC === "Sten")
+    ) {
+      Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+      setPlayerScore((p) => p + 1);
+      return "WIN";
+    }
 
     Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
-
-    setComputerScore(p => p + 1);
+    setComputerScore((p) => p + 1);
 
     return "LOOSE";
-}
-useEffect(() => {
-  if(playerScore === 2) {
-    dbService(playerScore, computerScore, "player", playerName);
-    setPlayerScore(0);
-    setComputerScore(0);
   }
+  useEffect(() => {
+    if (playerScore === 2) {
+      dbServiceInsertBestOf(playerScore, computerScore, "player", playerName)
+        .then(({ error }) => {
+          if (error) {
+            console.error("Insert error:", error.message);
+          } else {
+            console.log("Result saved");
+            setPlayerScore(0);
+            setComputerScore(0);
+          }
+        })
+        .catch((err) => {
+          console.error("Unexpected error:", err);
+        });
+    }
 
-  if(computerScore === 2) {
-    dbService(playerScore, computerScore, "computer", playerName);
-    setPlayerScore(0);
-    setComputerScore(0);
-  }
-},[playerScore, computerScore, playerName, setPlayerScore, setComputerScore]);
+    if (computerScore === 2) {
+      dbServiceInsertBestOf(playerScore, computerScore, "computer", playerName)
+        .then(({ error }) => {
+          if (error) {
+            console.error("Insert error:", error.message);
+          } else {
+            console.log("Result saved");
+            setPlayerScore(0);
+            setComputerScore(0);
+          }
+        })
+        .catch((err) => {
+          console.error("Unexpected error:", err);
+        });
+    }
+  }, [
+    playerScore,
+    computerScore,
+    playerName,
+    setPlayerScore,
+    setComputerScore,
+  ]);
 
-return { decideWinner }
+  return { decideWinner };
 }
